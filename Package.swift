d1b3a04e384b1ba45c6a549e67898c84a9d85ebb
Valerii Ivanov <valfrom@gmail.com>
2026-08-18 17:22:41 +0200

    Support iOS 16 package linking

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "MLXAudio",
-    platforms: [.macOS(.v14), .iOS(.v17)],
+    platforms: [.macOS(.v14), .iOS(.v16)],
     products: [
         // Core foundation library
         .library(name: "MLXAudioCore", targets: ["MLXAudioCore"]),
